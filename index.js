da56bb194351ef63b57b46d8f85725394f9ec8e8
mohieddine-kichli <mohieddine.kichli@gmail.com>
2022-09-10 14:10:02 +0300

    revise fetch javascript

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ results_button.addEventListener("click", get_results);
 function get_results() {
   const input = document.getElementById("name");
   const name = input.value;
-  const api_gender ='https://api.nationalize.io/?name=' + name;
+  const api_gender = 'https://api.genderize.io/?name=' + name;
   const api_age = 'https://api.agify.io/?name=' + name;
-  const api_nationalize = 'https://api.genderize.io/?name=' + name;
+  const api_nationalize = 'https://api.nationalize.io/?name=' + name;
 
   fetch(api_gender)
     .then((response) => response.json())
     .then((data) => {
 
-      console.log(data.country[0].country_id);
-
+      console.log(data.gender);
+      
   })
 
   fetch(api_age)
@@ -29,7 +29,7 @@ function get_results() {
     .then((response) => response.json())
     .then((data) => {
 
-      console.log(data.gender);
+      console.log(data.country[0].country_id);
 
   })
 }
