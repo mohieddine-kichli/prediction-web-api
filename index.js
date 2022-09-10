7b42112c64a0e950dcd7a305f0e71e1ed51506cc
mohieddine-kichli <mohieddine.kichli@gmail.com>
2022-09-10 12:55:59 +0300

    testing fetch command

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const input = "luis";
+const api_gender ='https://api.nationalize.io/?name=' + input;
+const api_age = 'https://api.agify.io/?name=' + input;
+const api_nationalize = 'https://api.genderize.io/?name=' + input;
+
+
+fetch(api_gender)
+  .then((response) => response.json())
+  .then((data) => {
+
+    console.log(data.country[0].country_id);
+
+})
+
+fetch(api_age)
+  .then((response) => response.json())
+  .then((data) => {
+
+    console.log(data.age);
+
+})
+
+
+fetch(api_nationalize)
+  .then((response) => response.json())
+  .then((data) => {
+
+    console.log(data.gender);
+
+})
